77589f6ecbe7828c2cee2f4ef1c89e14264c45a9
Misty <pyleaf@sohu.com>
2015-07-21 23:42:38 +0800

    init

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject funny "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.1.6"]
                  [http-kit "2.1.14"]
