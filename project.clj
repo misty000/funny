8a87f7a4872190376e7dfcbb80366f0d06d3e934
Misty <pyleaf@sohu.com>
2016-08-26 18:44:25 +0800

    0826

--- project.clj
+++ project.clj
@@ -8,11 +8,11 @@
 
                  [cheshire "5.6.3"]
                  [com.layerware/hugsql "0.4.7"]
-                 [com.h2database/h2 "1.4.192"]]
+                 [com.h2database/h2 "1.4.192"]
+                 [javax.servlet/servlet-api "2.5"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-ancient "0.6.10"]]
   :ring {:handler funny.handler/app}
   :main funny.handler
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
